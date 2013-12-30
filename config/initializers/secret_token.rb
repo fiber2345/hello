8ba4a90019ea4abb3102a965ed340fe17e9cd331
Pallavi <nchekka@gmail.com>
2013-12-30 15:31:53 -0800

    Intial project with bootstrap from twitter files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello1::Application.config.secret_key_base = 'a4ea8aefe8c79f8d29a7e69e3785fd444cd6f2dd40659546f06cb811f9ee7a82c2dc4ebad0ccfde84252f198ead1dc64b6dfef4b5bc36c5d76f28443e2dfa758'
